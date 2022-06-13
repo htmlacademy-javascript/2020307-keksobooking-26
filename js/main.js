11a2809ec92d499d72700e96b0682a1a35c8a0b0
MisterCloo <aosipov251@gmail.com>
2022-06-13 17:03:55 +0300

    Добавлена правка функции

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
+// function randomInteger(min, max) {
+//   const rand = min - 0 + Math.random() * (max - min + 1);
+//   return Math.round(rand);
+// }
+
+// randomInteger(1, 100);
+
+
 function randomInteger(min, max) {
-  const rand = min - 0 + Math.random() * (max - min + 1);
-  return Math.round(rand);
+  let randNumber = min - 1 + Math.random(0.5) * (max - min + 0.5);
+  return randNumber.toFixed(6);
 }
 
 randomInteger(1, 100);
