4e0a8a1f00f11ddab7224f1cc11a066896912a6a
MisterCloo <aosipov251@gmail.com>
2022-06-20 17:49:08 +0300

    function random number

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
-// function randomInteger(min, max) {
-//   const rand = min - 0 + Math.random() * (max - min + 1);
-//   return Math.round(rand);
-// }
-
-// randomInteger(1, 100);
-
-
-function randomInteger(min, max) {
-  let randNumber = min - 1 + Math.random(0.5) * (max - min + 0.5);
-  return randNumber.toFixed(6);
+function GetRandomNumber(min, max) {
+  const randomNumber = min - 1 + Math.random(0.5) * (max - min + 0.5);
+  return randomNumber.toFixed(6);
 }
 
-randomInteger(1, 100);
+GetRandomNumber(1, 100);
